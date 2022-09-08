8d063dfa40e5bb0ddd33c0fd510615ddf5a50657
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-09-08 11:04:52 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::array;
-
 fn main() {
     println!("Hello, world!");
 }
+
 //알고리즘의 복잡성
 
 //주어진 모든 숫자의 합을 계산
@@ -51,7 +50,7 @@ fn simple_calculate_array_sum(array: Vec<i32>) -> i32 {
 
     B - A + additional_nuber
 }
-
+//O(n^2)
 #[allow(dead_code)]
 fn not_so_simple_calculate(array: Vec<i32>) -> Vec<i32> {
     let mut array = array;
@@ -63,3 +62,48 @@ fn not_so_simple_calculate(array: Vec<i32>) -> Vec<i32> {
     }
     array
 }
+
+// #[allow(dead_code)]
+// fn might_be_simple_caculate(array: Vec<i32>) -> i32 {
+//     let mut total = 0;
+
+//     for number in array.iter() {
+//         let additional = if array.iter().position(|r| r == number).unwrap()
+
+//         total = total + number + additional;
+//     }
+//     total
+// }
+#[allow(dead_code)]
+fn realy_simple_caculate(array: Vec<i32>) -> i32 {
+    let mut total = 0;
+
+    for (index, number) in array.iter().enumerate() {
+        let additional = if index > 5 { 5 } else { 1 };
+
+        total = total + number + additional;
+    }
+    total
+}
+//n+n제곱에서 a를 2차원저긍로 0n 제곱
+//o(n!)
+#[allow(dead_code)]
+fn not_simple_calculate(array: Vec<i32>) -> Vec<i32> {
+    let mut array = array;
+    for number in array.iter() {
+        println!("{}", number);
+    }
+
+    for i in 0..array.len() {
+        for j in 0..array.len() {
+            array[i] = array[i] + array[j];
+        }
+    }
+    array
+}
+//O(1)
+//O(log n)
+//O(n)
+//O(n^2)
+//O(n^3)
+//O(n!)
